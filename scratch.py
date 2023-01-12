a2daa64e0ad62d1ce2a265fea246fd2bdc3fc087
David Kim <david.kim@saberastro.com>
2023-01-12 23:10:06 +1100

    impressive

---
+++ scratch.py
@@ -0,0 +1,16 @@
+# Understanding Python and REST APIs.
+# https://realpython.com/api-integration-in-python/#:~:text=REST%20APIs%20provide%20access%20to%20web%20service%20data%20through%20public%20web%20URLs.&text=This%20URL%20allows%20you%20to,URL%20and%20processing%20the%20response.
+
+
+# Personal Access Token for UP Bank and getting started:
+# up:yeah:7heHOrUGAWTt6tDxt4v376MfvNDd7cRpQZwrM7qZbQ7YqrobCmLvUrQipqFhgTQrIHvIQT058PYZXMNUmPIJmKsXxP6qaL6pzGBs4bUzgqQVLi3M5oOSHqJjQCVXDVKP
+# https://developer.up.com.au/#getting-started
+
+# Python & APIs: A Winning Combo for Reading Public Data:
+# https://realpython.com/python-api/#:~:text=API%20stands%20for%20application%20programming,in%20many%20forms%20or%20shapes.
+
+
+# TO-DO list:
+# idk how to get black to work properly
+# how to start virtual environment everytime i open vs code or a new terminal
+#
